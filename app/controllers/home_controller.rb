604ce5e83bab03f8d0134267f7e2093bcd0de73e
Kang Hyeong Min <humn9136@gmail.com>
2017-02-25 10:30:20 +0000

    index page, cadidate page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,34 @@
 class HomeController < ApplicationController
   def index
+    @candidates = Candidate.all
+    @articles = Article.all
+    
+  
+  end
+  
+  def like
+    article=Article.find(params[:article])
+    article.like= article.like + 1
+    article.save
+    puts 'here like called!'
+    redirect_to "/"
+  
+  end
+  
+  def unlike
+    article=Article.find(params[:article])
+    puts article
+    article.unlike=article.unlike + 1
+    article.save
+    puts 'unlike called!'
+    redirect_to "/"
+  end
+  
+  def candidate
+    @candidate=Candidate.find(params[:candidate_id])
+    
+    
+    
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
+  # get 'home/index' => 'home#index'
+  root 'home#index'
+  get 'home/like/:article' => 'home#like'
+  get 'home/unlike/:article' => 'home#unlike'
+  get 'home/candidate/:candidate_id' => 'home#candidate'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
