61e33887df564a73ac6c073aa01fed866cefc823
Kang Hyeong Min <humn9136@gmail.com>
2017-02-25 10:32:10 +0000

    crawler code

---
+++ public/crawl.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'nokogiri'
+require 'uri'
+require 'cgi'
+
+name = "이재명"
+num = 0
+#0 : 한겨레, 1 : 조선일보
+post = 1
+
+de_name = CGI::escape(name)
+
+if post == 0
+	one = Nokogiri::HTML(open("http://search.hani.co.kr/Search?command=query&keyword=#{de_name}&media=news&sort=d&period=all&datefrom=2000.01.01&dateto=2017.02.25&pageseq=#{num}"))
+	t = one.xpath("//dt//a")
+	t.each do |i|
+		puts i.inner_text
+		puts i['href']
+	end
+elsif post == 1
+	one = Nokogiri::HTML(open("http://search.chosun.com/search/news.search?query=#{de_name}&pageno=#{num}&orderby=&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=&categoryd2=&c_scope=news&sdate=&edate=&premium="))
+	t = one.xpath("//dt//a")
+	t.each do |i|
+		puts i.inner_text
+		puts i['href']
+	end
+
+end
