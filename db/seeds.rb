22a1df475f45b85ffe19a0ff0d66f695b4739f76
kimiseob <dltjq0730@unist.ac.kr>
2017-04-02 17:34:47 +0000

    fix css, index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Article.create(title: "sample_title", like:3, unlike:2, link:"sample_link")
+Article.create(title: "sample_title", like:5, unlike:1, link:"sample_link")
+Article.create(title: "sample_title", like:12, unlike:30, link:"sample_link")
+Article.create(title: "sample_title", like:44, unlike:5, link:"sample_link")
+Article.create(title: "sample_title", like:100, unlike:3, link:"sample_link")
+Article.create(title: "sample_title", like:33, unlike:21, link:"sample_link")
+Article.create(title: "sample_title", like:1, unlike:2, link:"sample_link")
 Candidate.create(name: "문재인", party: "더불어민주당", link: "http://moonjaein.com/", keyword: "국민:100,복지:80,변화:70,문화:60,성장:50,공존:10,경제민주화:10,안보:5", rank: 31, rank1ago: 33, rank2ago: 32, rank3ago: 34)
+Candidate.create(name: "안철수", party: "국민의당", link: "http://moonjaein.com/", keyword: "국민:100,복지:80,변화:70,문화:60,성장:50,공존:10,경제민주화:10,안보:5", rank: 31, rank1ago: 33, rank2ago: 32, rank3ago: 34)
