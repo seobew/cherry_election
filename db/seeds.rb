f4071b59d7069d0b93716a4a6ab1d48aa53e5141
Kang Hyeong Min <humn9136@gmail.com>
2017-04-27 17:17:32 +0000

    좋아요, 싫어요 값이 없으면(nil) 안보이는 대신 0이 뜨게

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Article.create(title: "sample_title", like:3, unlike:2, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:5, unlike:1, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:12, unlike:30, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:44, unlike:5, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:100, unlike:3, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:33, unlike:21, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
-Article.create(title: "sample_title", like:1, unlike:2, link:"sample_link", publisher: "publisher_sample", article_date: "date_sample")
 Candidate.create(name: "문재인", number:1, image:"moonjaein.jpg", party: "더불어민주당", link: "http://policy.nec.go.kr/skin/doc.html?fn=20170416205303235_1.pdf&rs=/preview/html/20170428/", keyword: "멱살:6650,1번가:9055,천안:5725,성남:5425,유세차량사고:4552,사고:3413,문재수:3325,정책본부:4026,하하하:3089,이명박:4026,차별:5000,일자리:5012,탄핵:3046,정권교체:4022,적폐:500", rank: 31, rank1ago: 33, rank2ago: 32, rank3ago: 34)
 Candidate.create(name: "홍준표", number:2, image:"hongjunpye.jpg", party: "자유한국당", link: "http://policy.nec.go.kr/skin/doc.html?fn=20170416202457827_1.pdf&rs=/preview/html/20170428/", keyword: "빅데이터트랜드분석:4190,빅데이터트랜드:3650,방송기자클럽초청회:2883,문재인1번가:2113,홍준표서문시장:2058,유승민학력:1786, 위키리스크:1423,희열:804,청와대:3000,가산점:2012,그리스:300,재판:100, 돼지흥분제:4444,사람:1000,좌파:1011,탄핵:800", rank: 31, rank1ago: 33, rank2ago: 32, rank3ago: 34)
 Candidate.create(name: "안철수", number:3, image:"ann.jpg", party: "국민의당", link: "http://policy.nec.go.kr/skin/doc.html?fn=20170416202520641_1.pdf&rs=/preview/html/20170428/", keyword: "구글빅데이터:4175,안철수부산유세:2011,mb아바타:1970,안철수딸안설희:1870,안철수가사도우미:1729,hermuhan@naver.com:1661,안철수광화문:1611,미세먼지:500,대기업:1006,육성:2034,4차산업:3000,집권:2066,정치인:2041,책임:300,개헌:1028,안보:1021,일자리:3000 ", rank: 31, rank1ago: 33, rank2ago: 32, rank3ago: 34)
