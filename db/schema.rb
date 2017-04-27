484a7b817964bb6c39e14b1354201bdb1ff1eb76
kimiseob <dltjq0730@unist.ac.kr>
2017-04-27 12:40:32 +0000

    add name, provider, uid to devise user for facebook login and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417104539) do
+ActiveRecord::Schema.define(version: 20170427123852) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 20170417104539) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
