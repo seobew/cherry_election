8efb8165098068de43d16085231a495c887a806a
kimiseob <dltjq0730@unist.ac.kr>
2017-04-27 12:46:33 +0000

    set config of facebook id, secret to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -52,6 +52,7 @@ Devise.setup do |config|
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [:email]
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
