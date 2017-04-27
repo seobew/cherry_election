eaeb9da760da561a6c67cd7de7337c818abda5ba
kimiseob <dltjq0730@unist.ac.kr>
2017-04-27 12:48:09 +0000

    say user model to use omniauth_provider:facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
   has_many :userfavors
   has_many :articles, through: :userfavors
 end
