64d4252e80f90a312658cc6a12736e5d78db3917
kimiseob <dltjq0730@unist.ac.kr>
2017-04-28 02:48:05 +0000

    add login part on header, hide app_id, secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,7 +249,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "1506670346023472", "440f23f630cccf8eba33b085e82aa18c", callback_url: "https://cherryelection-iseob-kimiseob.c9users.io/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"], callback_url: "https://cherryelection-iseob-kimiseob.c9users.io/users/auth/facebook/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
